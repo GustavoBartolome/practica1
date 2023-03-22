e5f1c563c291ec147db4c7a4a77fbb581b3ef195
GustavoBartolome <126710495+GustavoBartolome@users.noreply.github.com>
2023-03-22 17:21:34 +0100

    Update principal.java

--- principal.java
+++ principal.java
@@ -5,7 +5,7 @@ public class principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo");
-
+		System.out.println("hola a todos");
 	}
 
 }
